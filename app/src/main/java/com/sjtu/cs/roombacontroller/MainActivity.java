32902c7eeb034366283f7f9def69a678d36d3f5f
StellEdge <1030110910@qq.com>
2018-04-26 22:30:27 +0800

    core of the controller,starting a new project.

---
+++ app/src/main/java/com/sjtu/cs/roombacontroller/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sjtu.cs.roombacontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

e4834c5e666ca06eae5de056ebc7e7ecf3996b37
AsanoKiri <925536158@qq.com>
2018-05-01 20:49:02 +0800

    圆形操控已经全部写完了。 @陈欣然：我在MainActivity里获得了屏幕尺寸（单位是像素（p）)，你看看能不能用这个改改你画的圆，我算法设计的是圆心在屏幕正中心（高和宽都/2），可见圆的半径是宽/3，容错圆的半径是宽/2（也就是屏幕横向到头）。(如果有按钮的话，一定要整体位于容错圆外面；如果要画滑条的操作模式，也可以利用这些个尺寸.如果你要写滑条的算法，就把左右轮速度传给line 17的lspeed和rspeed,然后我加一个输出选择就行了。）
    至于另一种操控方式，需要再写的地方就是我们写的算法和界面了，现在还没有做。

--- app/src/main/java/com/sjtu/cs/roombacontroller/MainActivity.java
+++ app/src/main/java/com/sjtu/cs/roombacontroller/MainActivity.java
@@ -2,22 +2,39 @@ package com.sjtu.cs.roombacontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.MotionEvent;
-public class MainActivity extends AppCompatActivity {
+import java.lang.Math;
+
+import static java.lang.Math.PI;
+
+public class MainActivity extends AppCompatActivity {///李桐：希望我们能弄个text输出一下当前速度和半径
+    int widthPixels;//litong:屏幕尺寸
+    int heightPixels;
+    int speed, radius;
+    //int lspeed, rspeed;
+
+    Location mlocation = new Location();
+
+    private void measure(){//这个函数用来获得屏幕尺寸
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        this.widthPixels = metrics.widthPixels;
+        this.heightPixels = metrics.heightPixels;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("send", "emmmmm");
+        Log.d("send", "You can text here");
+        measure();//李桐：这一行我写的……
         // to tong 在连接完后可以用这个log测试一下鼠标移动输出指令的工作情况
     }
 
-    public boolean onTouchEvent(View view, MotionEvent event) {//李桐：就这样了这块儿
-        Location mlocation = new Location();
-
+    public boolean onTouchEvent(View view, MotionEvent event) {//李桐：触摸监听，更新mlocation中的坐标
         switch(event.getAction()){
             case MotionEvent.ACTION_DOWN: case MotionEvent.ACTION_MOVE:
                 mlocation.update(event.getX(), event.getY());
@@ -26,7 +43,36 @@ public class MainActivity extends AppCompatActivity {
             case MotionEvent.ACTION_UP:
                 mlocation.update_condition(false);
         }
+
+        calculate(mlocation);
+        BluetoothSend(" ",CirSend(this.speed, this.radius));
+        //李桐：这里是给蓝牙传输，tag我用了空字符串
         return true;
+    }
+
+    //李桐：下面是计算速度,对应圆盘操作模式,更新this.speed和this.radius
+    private void calculate(Location mlocation){
+        double x = mlocation.x() - (this.widthPixels/2);
+        double y = mlocation.y() - (this.heightPixels/2);
+        final double R = this.widthPixels/3;//R is the radius of the visible circle
+        double a, r;
+
+        if (!mlocation.conditon()){
+            this.speed = 0;
+            this.radius = 1;
+        }else{
+            r = Math.sqrt(x*x+y*y);
+            a = -Math.signum(y)*Math.atan(x*1.0/y);
+
+            if (r>R && r<this.widthPixels/2){//widthPixels is the max valiad radius
+                this.speed = (int)Math.signum(y)*500;
+            }else {
+                if (r <= R) {
+                    this.speed = (int) (Math.signum(y) * r * 500 / R);
+                }else this.speed = 0;
+            }
+            this.radius =(int)(2000*a/(PI/300));
+        }
     }
 
     // Created by hd on 2018/4/30.
